467fe24ef84bd7c636e8e52d0283f586460b1de2
FrederikCres <62345365+FrederikCres@users.noreply.github.com>
2021-12-12 21:12:06 +0100

    Update tokenContract.sol

--- tokenContract.sol
+++ tokenContract.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.21;
 import "./EIP20Interface.sol";
 
 
-contract <-- choose token name here -->  is EIP20Interface {
+contract <-- insert token name here -->  is EIP20Interface {
 
     uint256 constant private MAX_UINT256 = 2**256 - 1;
     mapping (address => uint256) public balances;
